5e74e5f41980f0c65830b7777752aad1148c4ad2
rahulraj0402 <rahul3004rt@gmail.com>
2023-12-11 20:15:12 +0530

    string solution added

--- src/Arrays/BuildArrayFromPermutationLeetcode1920.java
+++ src/Arrays/BuildArrayFromPermutationLeetcode1920.java
@@ -2,6 +2,18 @@ package Arrays;
 
 public class BuildArrayFromPermutationLeetcode1920 {
     public static void main(String[] args) {
-        System.out.println("hey");
+      int[] nums = {0,2,1,5,3,4};
+      buildArray(nums);
+      
+
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for(int i = 0 ; i < nums.length ; i++){
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
     }
 }
